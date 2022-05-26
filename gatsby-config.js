1b8f2146d9d804cae486897e9b511603ba0c97a4
patrickhthomas <patrick@sendhelp.biz>
2022-05-26 11:50:02 -0700

    updates favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Patrick Thomas Digital Design`,
+    description: `Digital Design work, UI UX, Graphic Design, Web Design by Patrick Thomas.`,
+    author: `Patrick Thomas`,
+    siteUrl: `https://patrickthomas.design/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -31,15 +31,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Patrick Thomas Digital Design`,
+        short_name: `Patrick Thomas`,
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/myLogo.png`, // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
